55c7a4ebbc5e815dcd62ec8cc3ecba91e7817727
TheWinningGlory <87405311+TheWinningGlory@users.noreply.github.com>
2021-07-20 18:43:27 -0700

    Update loginandregister.py

--- loginandregister.py
+++ loginandregister.py
@@ -1,18 +1,25 @@
 from tkinter import *
+import os
 
 def register_user():
     username_info = username.get()
     password_info = password.get()
 
-    file = open(username_info + ".txt", "w")
-    file.write(username_info)
+    file = open(username_info, "w")
+    file.write(username_info + "\n")
     file.write(password_info)
-
+    file.close()
     usernameEntry.delete(0, END)
     passwordEntry.delete(0, END)
 
-    Label(text = "Registration successful")
+    Label(screen1, text = "Registration successful", fg = "green", font = ("calibri", 11)).pack()
+def login_verify():
+    username1 = username_verify.get()
+    password1 = password_verify.get()
+    username_entry1.delete(0, END)
+    password_entry1.delete(0, END)
 def register():
+    global screen1
     screen1 = Toplevel(screen)
     screen1.title("Register")
     screen1.geometry("300x250")
@@ -36,7 +43,31 @@ def register():
     Button(screen1, text = "Register", width = "10", height = "1", command = register_user).pack()
 
 def login():
-    print("Login session started")
+    global screen2
+    screen2 = Toplevel(screen)
+    screen2.title("Login")
+    screen2.geometry("300x250")
+    Label(screen2, text = "Please enter details below to login").pack()
+    Label(screen2, text = "").pack()
+    global username_verify
+    global password_verify
+
+    username_verify = StringVar()
+    password_verify = StringVar()
+
+    global username_entry1
+    global password_entry1
+
+    Label(screen2, text = "Username * ").pack()
+    username_entry1 = Entry(screen2, textvariable = username_verify)
+    username_entry1.pack()
+    Label(screen2, text = "").pack()
+    Label(screen2, text = "Password * ").pack()
+    password_entry1 = Entry(screen2, textvariable = password_verify)
+    password_entry1.pack()
+    Label(screen2, text = "").pack()
+    Button(screen2, text = "Login", width = 10, height = 1, command = login_verify)
+
 
 def mainScreen():
     global screen
