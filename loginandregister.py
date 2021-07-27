da68d4292250dbfdfcf69cb83a9fc62fef4dd0d3
TheWinningGlory <87405311+TheWinningGlory@users.noreply.github.com>
2021-07-27 10:31:03 -0700

    Update loginandregister.py

--- loginandregister.py
+++ loginandregister.py
@@ -1,81 +1,62 @@
 from tkinter import *
 import os
 
-def delete2(): 
+def delete2():
   screen3.destroy()
 
-def delete3(): 
+def delete3():
   screen4.destroy()
 
-def delete4(): 
-  screen5.destroy()
-
 def bank_account_code():
-    createdAccountusername = input("Enter username that you want to create:\t")
-    createdAccountpassword = input("Enter password that you want to create:\t")
 
-    class bankAccount:
-        def __init__(self, money_in_account, withdrawl, deposit, username, password, ammount_of_deposit,
-                     ammount_of_withdrawl, balance):
-            self.money_in_account = money_in_account
-            self.withdrawl = withdrawl
-            self.deposit = deposit
-            self.username = username
-            self.password = password
-            self.ammount_of_deposit = ammount_of_deposit
-            self.ammount_of_withdrawl = ammount_of_withdrawl
-            self.balance = balance
+  class bankAccount:
+      def __init__(self, money_in_account, withdrawl, deposit, ammount_of_deposit, ammount_of_withdrawl, balance):
+          self.money_in_account = money_in_account
+          self.withdrawl = int(input("How much do you want to withdrawl:\t"))
+          self.deposit = int(input("How much do you want to deposit:\t"))
+          self.balance = balance
 
-        ammount_of_deposit = int(input("How much do you want to deposit:\t"))
-        ammount_of_withdrawl = int(input("How much do you want to withdrawl:\t"))
-        username = input("Enter username:\t")
-        password = input("Enter password:\t")
-        money_in_account = 0
-        balance = money_in_account + ammount_of_deposit - ammount_of_withdrawl
-        passwordChecker = len(createdAccountpassword) > 10
-        one_more_password_check = createdAccountpassword != createdAccountusername
-        finalBalance = f"Remaining Ammount: ${balance}"
-        how_many_times = 0
+      money_in_account = 0
+      balance = money_in_account + deposit - withdrawl
+      finalBalance = f"Remaining Ammount: ${balance}"
+      how_many_times = 0
+  
+      print("""
+      Intiallalizing...
+      Loading Account...
+      Account Balance: $0
+      """)
+      transaction = input("""
+      make a deposit - d
+      make a withdrawl - w
+      Enter transaction:\t
+      """)
 
-        while passwordChecker and one_more_password_check is True:
-            while createdAccountusername != username and createdAccountpassword != password and balance > ammount_of_withdrawl:
-                print("Either username or password is not correct. Please try again.")
-                username = input("Enter username:\t")
-                password = input("Enter password:\t")
-                if createdAccountusername == username and createdAccountpassword == password:
-                    print("""
-            Intiallalizing...
-            Loading Account...
-            Account Balance = $0
-            """)
-                transaction = input("""
-            make a deposit - d
-            make a withdrawl - w
-            Enter transaction:\t
-            """)
-                ammount_of_deposit = int(input("How much do you want to deposit:\t"))
-                ammount_of_withdrawl = int(input("How much do you want to withdrawl:\t"))
-                balance = money_in_account + ammount_of_deposit - ammount_of_withdrawl
-                if transaction.lower == "w" and ammount_of_withdrawl <= balance:
-                    print("Transaction succesful")
-                elif transaction.lower() == "w" and ammount_of_withdrawl > balance:
-                    print("Sorry but you don't have that much money in your account!")
-                    break
-                else:
-                    print("Error please try again")
-            while how_many_times < 1:
-                print(finalBalance)
-                print("Thank You for doing your transaction with iBank!")
-                how_many_times += 1
-
+    
+      if transaction.lower == "w":
+        print(self.withdrawl)
+      elif transaction.lower == "d":
+        print(self.deposit)
+      balance = money_in_account + deposit - withdrawl
+      if transaction.lower == "w" and self.withdrawl <= balance:
+        print("Transaction succesful")
+      elif transaction.lower() == "w" and withdrawl > balance:
+        print("Sorry but you don't have that much money in your account!")
+      else:
+        print("Error please try again")
+      while how_many_times < 1:
+        print(finalBalance)
+        print("Thank You for doing your transaction with iBank!")
+        how_many_times += 1
 
 def session():
-    screen8 = Toplevel(screen)
-    screen8.title("Dashboard")
-    screen.geometry("350x200")
-    Label(screen8, text = "Welcome to Dashboard").pack()
-    Button(screen8, text = "Open Banking App", command = bank_account_code).pack()
-
+  global screen5
+  screen5 = Toplevel(screen)
+  screen5.title("Dashboard")
+  screen5.geometry("400x400")
+  Label(screen5, text = "Welcome to Dashboard").pack()
+  Button(screen5, text = "Open Banking App", command = bank_account_code).pack()
+  
 
 def login_success():
   global screen3
@@ -84,7 +65,8 @@ def login_success():
   screen3.geometry("150x100")
   Label(screen3, text = "Login success").pack()
   Button(screen3, text = "OK", command = delete2).pack()
-
+  if login_success:
+    session()   
 def password_not_recognized():
   global screen4
   screen4 = Toplevel(screen)
@@ -103,93 +85,94 @@ def user_not_found():
   Button(screen5, text = "OK", command = delete4).pack()
 
 def register_user():
-    username_info = username.get()
-    password_info = password.get()
+  username_info = username.get()
+  password_info = password.get()
 
-    file = open(username_info, "w")
-    file.write(username_info + "\n")
-    file.write(password_info)
-    file.close()
-    usernameEntry.delete(0, END)
-    passwordEntry.delete(0, END)
+  file = open(username_info, "w")
+  file.write(username_info + "\n")
+  file.write(password_info)
+  file.close()
+  usernameEntry.delete(0, END)
+  passwordEntry.delete(0, END)
 
-    Label(screen1, text = "Registration successful", fg = "green", font = ("calibri", 11)).pack()
+  Label(screen1, text = "Registration successful", fg = "green", font = ("calibri", 11)).pack()
 def login_verify():
-    username1 = username_verify.get()
-    password1 = password_verify.get()
-    username_entry1.delete(0, END)
-    password_entry1.delete(0, END)
+  username1 = username_verify.get()
+  password1 = password_verify.get()
+  username_entry1.delete(0, END)
+  password_entry1.delete(0, END)
 
-    list_of_files = os.listdir()
-    if username1 in list_of_files:
-        file1 = open(username1, "r")
-        verify = file1.read().splitlines()
-        if password1 in verify:
-            login_success()
-        else:
-            password_not_recognized()
+  list_of_files = os.listdir()
+  if username1 in list_of_files:
+    file1 = open(username1, "r")
+    verify = file1.read().splitlines()
+    if password1 in verify:
+      login_success()
     else:
-        user_not_found()
+      password_not_recognized()
+  else:
+    user_not_found()
 def register():
-    global screen1
-    screen1 = Toplevel(screen)
-    screen1.title("Register")
-    screen1.geometry("300x250")
+  global screen1
+  screen1 = Toplevel(screen)
+  screen1.title("Register")
+  screen1.geometry("300x250")
 
-    global username
-    global password
-    global usernameEntry
-    global passwordEntry
-    username = StringVar()
-    password = StringVar()
+  global username
+  global password
+  global usernameEntry
+  global passwordEntry
+  username = StringVar()
+  password = StringVar()
 
-    Label(screen1, text = "Please enter details below").pack()
-    Label(screen1, text = "").pack()
-    Label(screen1, text = "Username * ").pack()
-    usernameEntry = Entry(screen1, textvariable = username)
-    usernameEntry.pack()
-    Label(screen1, text = "Password * ").pack()
-    passwordEntry = Entry(screen1, textvariable = password)
-    passwordEntry.pack()
-    Label(screen1, text = "").pack()
-    Button(screen1, text = "Register", width = "10", height = "1", command = register_user).pack()
+  Label(screen1, text = "Please enter details below").pack()
+  Label(screen1, text = "").pack()
+  Label(screen1, text = "Username * ").pack()
+  usernameEntry = Entry(screen1, textvariable = username)
+  usernameEntry.pack()
+  Label(screen1, text = "Password * ").pack()
+  passwordEntry = Entry(screen1, textvariable = password)
+  passwordEntry.pack()
+  Label(screen1, text = "").pack()
+  Button(screen1, text = "Register", width = "10", height = "1", command = register_user).pack()
 
 def login():
-    global screen2
-    screen2 = Toplevel(screen)
-    screen2.title("Login")
-    screen2.geometry("300x250")
-    Label(screen2, text = "Please enter details below to login").pack()
-    Label(screen2, text = "").pack()
-    global username_verify
-    global password_verify
+  global screen2
+  screen2 = Toplevel(screen)
+  screen2.title("Login")
+  screen2.geometry("300x250")
+  Label(screen2, text = "Please enter details below to login").pack()
+  Label(screen2, text = "").pack()
+  global username_verify
+  global password_verify
 
-    username_verify = StringVar()
-    password_verify = StringVar()
+  username_verify = StringVar()
+  password_verify = StringVar()
 
-    global username_entry1
-    global password_entry1
+  global username_entry1
+  global password_entry1
 
-    Label(screen2, text = "Username * ").pack()
-    username_entry1 = Entry(screen2, textvariable = username_verify)
-    username_entry1.pack()
-    Label(screen2, text = "").pack()
-    Label(screen2, text = "Password * ").pack()
-    password_entry1 = Entry(screen2, textvariable = password_verify)
-    password_entry1.pack()
-    Label(screen2, text = "").pack()
-    Button(screen2, text = "Login", width = 10, height = 1, command = login_verify).pack()
+  Label(screen2, text = "Username * ").pack()
+  username_entry1 = Entry(screen2, textvariable = username_verify)
+  username_entry1.pack()
+  Label(screen2, text = "").pack()
+  Label(screen2, text = "Password * ").pack()
+  password_entry1 = Entry(screen2, textvariable = password_verify)
+  password_entry1.pack()
+  Label(screen2, text = "").pack()
+  Button(screen2, text = "Login", width = 10, height = 1, command = login_verify).pack()
 
 
 def mainScreen():
-    global screen
-    screen = Tk()
-    screen.geometry("300x250")
-    screen.title("Banking Application 1.0")
-    Label(text = "Banking Application 1.0", bg = "grey", width = "300", height = "2", font = ("Calibri", 13)).pack()
-    Label(text = "").pack()
-    Button(text = "Login", height = "2", width = "30", command = login).pack()
-    Label(text = "").pack()
-    Button(text = "Register", height = "2", width = "30", command = register).pack()
-    screen.mainloop()
+  global screen
+  screen = Tk()
+  screen.geometry("300x250")
+  screen.title("Banking Application 1.0")
+  Label(text = "Banking Application 1.0", bg = "grey", width = "300", height = "2", font = ("Calibri", 13)).pack()
+  Label(text = "").pack()
+  Button(text = "Login", height = "2", width = "30", command = login).pack()
+  Label(text = "").pack()
+  Button(text = "Register", height = "2", width = "30", command = register).pack()
+  screen.mainloop()
+
 mainScreen()
